5aea72910d3b1d0278f03df5a6e0e52ca02420f3
nathaliafleite <nathalia.leite2@gmail.com>
2023-08-02 15:20:30 +0100

    first commit

---
+++ src/nathaliafleite/temp_converter.cljs
@@ -0,0 +1,38 @@
+(ns nathaliafleite.temp-converter
+  (:require [goog.dom :as gdom]
+            [goog.events :as gevents]))
+
+(defn f->c [deg-f]
+  (/ (- deg-f 32) 1.8))
+
+(defn c->f [deg-c]
+  (+ (* deg-c 1.8) 32))
+
+(def celsius-radio (gdom/getElement "unit-c"))
+(def fahrenheit-radio (gdom/getElement "unit-f"))
+(def temp-input (gdom/getElement "temp"))
+(def output-target (gdom/getElement "temp-out"))
+(def output-unit-target (gdom/getElement "unit-out"))
+
+(defn get-input-unit []
+  (if (.-checked celsius-radio)
+    :celsius
+    :fahrenheit))
+
+(defn get-input-temp []
+  (js/parseInt (.-value temp-input)))
+
+(defn set-output-temp [temp]
+  (gdom/setTextContent output-target
+                       (.toFixed temp 2)))
+
+(defn update-output [_]
+  (if (= :celsius (get-input-unit))
+    (do (set-output-temp (c->f (get-input-temp)))
+        (gdom/setTextContent output-unit-target "F"))
+    (do (set-output-temp (f->c (get-input-temp)))
+        (gdom/setTextContent output-unit-target "C"))))
+
+(gevents/listen temp-input "keyup" update-output)
+(gevents/listen celsius-radio "click" update-output)
+(gevents/listen fahrenheit-radio "click" update-output)
---
+++ test/nathaliafleite/temp_converter_test.cljs
@@ -0,0 +1,10 @@
+(ns nathaliafleite.temp-converter-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [nathaliafleite.temp-converter :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
